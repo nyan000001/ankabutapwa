52c64b5be13b38a24b92c0e5a4e8ca526f39c2b8
nyan000001 <150596189+nyan000001@users.noreply.github.com>
2026-07-24 20:00:34 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,21 @@
-self.addEventListener('push', async (event) => {
-	if(!event.data) return;
-	const payload = event.data.json();
-	self.registration.showNotification(payload.title, { body:payload.body });
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(clients.claim()));
+self.addEventListener("fetch", event => {
+	const url = new URL(event.request.url);
+	if(event.request.method == 'POST' && url.pathname == '/share') {
+		event.respondWith(async event => {
+			const clients = await self.clients.matchAll({ type:'window', includeUncontrolled:true });
+			if(clients.length) {
+				const client = clients[0];
+				await client.focus();
+				return Response.redirect(client.url, 303);
+			}
+			return Response.redirect('/', 303);
+		});
+	}
+});
+self.addEventListener('fetch', event => event.respondWith(fetch(event.request)));
+self.addEventListener('push', event => {
+	const { title, body } = event.data.json();
+	event.waitUntil(self.registration.showNotification(title, { body }));
 });
