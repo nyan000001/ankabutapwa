e33d7ad707494cfaf8f3cb6cf4adaf7b4dde70a4
nyan000001 <150596189+nyan000001@users.noreply.github.com>
2026-08-10 19:37:45 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -2,19 +2,20 @@ self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(clients.claim()));
 self.addEventListener("fetch", event => {
 	const url = new URL(event.request.url);
-	if(event.request.method == 'POST' && url.pathname == '/share') {
-		event.respondWith(async event => {
-			const clients = await self.clients.matchAll({ type:'window', includeUncontrolled:true });
-			if(clients.length) {
-				const client = clients[0];
-				await client.focus();
-				return Response.redirect(client.url, 303);
-			}
-			return Response.redirect('/', 303);
-		});
-	}
+	if(event.request.method != 'POST' || url.pathname != '/share') return;
+	event.respondWith(async () => {
+		const clients = await self.clients.matchAll({ type:'window', includeUncontrolled:true });
+		if(clients.length) {
+			const formData = await event.request.formData(); 
+			const images = formData.getAll('images');
+			const client = clients[0];
+			client.postMessage({ type:'image', file:images });
+			await client.focus();
+			return Response.redirect(client.url, 303);
+		}
+		return Response.redirect('/', 303);
+	});
 });
-self.addEventListener('fetch', event => event.respondWith(fetch(event.request)));
 self.addEventListener('push', event => {
 	const { title, body } = event.data.json();
 	event.waitUntil(self.registration.showNotification(title, { body }));
